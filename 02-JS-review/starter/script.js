c3b655f551eaf3611e4e50d8fcda1dce91448686
Seoyeon Um <seoyeon.ahora@gmail.com>
2025-08-24 21:25:17 +0900

    📝 chore: add examples of essential JS for React
    - practiced ternery operator (instead of if/else statements)
    - followed along with instructor examples

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -145,7 +145,7 @@ function getBook(id) {
 
 // Destructuring
 
-const book = getBook(1);
+const book = getBook(2);
 book;
 
 // const title = book.title;
@@ -184,5 +184,10 @@ updateBook;
 // template literals
 const summary = `${title}, a ${pages}-pages long book, was written by ${author} and publish in ${
   publicationDate.split('-')[0]
-} is a book`;
+}. The book has ${hasMovieAdaptation ? '' : 'not'} been adapted as a movie`;
 summary;
+
+// terneries instead of if/else statements
+const pagesRange = pages > 1000 ? 'over a thousand' : 'less than 1000';
+pagesRange;
+console.log(`The book has ${pagesRange} pages`);
